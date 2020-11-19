052ba9ba9e40276561c4786ff41ee9e3dd5cc24f
Thandar <juliashwesin@gmail.com>
2020-11-19 21:38:50 +0630

    Add project files.

---
+++ Controllers/IceCreamController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IceCreamShopDemo.Controllers
+{
+    public class IceCreamController : Controller
+    {
+        // GET: IceCream
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
